5753e4cc9c9c51d62d212019ee2e7b76b52a7f41
NorcyChen <norcychen@tencent.com>
2020-06-17 13:03:30 +0800

    AutoPush

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # XcodeSnippets
 Xcode 代码块备份
+
+直接执行 `restore.sh`
+
+或手动输入
+
+```sh
+mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+cd ~/Library/Developer/Xcode/UserData
+git clone https://github.com/Norcy/XcodeSnippets.git
+echo "done"
+```
--- restore.sh
+++ restore.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-
-# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
-
-SRC_HOME=`pwd`
-ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+cd ~/Library/Developer/Xcode/UserData
+git clone https://github.com/Norcy/XcodeSnippets.git
 echo "done"
